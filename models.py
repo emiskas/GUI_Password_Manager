430299bea0a2b8452b2c51d67e62c845390eb433
Emilis Paulauskas <emilispaulauskas279@gmail.com>
2025-01-05 16:51:11 +0200

    Remove unused import.

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-from cryptography.fernet import Fernet
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
